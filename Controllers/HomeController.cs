3461c4f95f0cf003c9bf2a1bea20bd1828f3b270
Rodrigo Eltz <rpetrye@hotmail.com>
2020-02-05 14:58:55 -0500

    exercise 3 done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using LanguageProjectAsp.Models;
+using System.IO;
 
 namespace LanguageProjectAsp.Controllers
 {
@@ -28,8 +29,9 @@ namespace LanguageProjectAsp.Controllers
             return View();
         }
 
-        //Method to retrieve the ViewResult with the view Exercise1. 
-        //@Rodrigo Eltz
+        /** Method to retrieve the ViewResult with the view Exercise1. 
+        @Rodrigo Eltz
+    */
         public ViewResult Exercise1()
         {
             // Names of all the columns
@@ -38,6 +40,36 @@ namespace LanguageProjectAsp.Controllers
                 "VALUE", "STATUS", "SYMBOL", "TERMINATED", "DECIMALS"};
 
             return View(columns);
+        }
+
+        //Method to retrieve the ViewResult for the Exercise 3
+        //@Rodrigo Eltz
+        public ViewResult Exercise3()
+        {
+            return View(readFromCsv()); ;
+        }
+
+        /// Method that reads the 5 first records from the CSV and creates a list of objects of type Record.
+        private List<Record> readFromCsv()
+        {
+            var filePath = "D:\\13100262.csv";
+            List<Record> fiveRecords = new List<Record>();
+            using (StreamReader stream = new StreamReader(filePath))
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    if (i == 0)
+                    {
+                        stream.ReadLine();
+                    }
+                    else
+                    {
+                        fiveRecords.Add(Record.FromCsv(stream.ReadLine()));
+                    }
+                }
+            }
+            System.Diagnostics.Debug.WriteLine(fiveRecords);
+            return fiveRecords;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/Record.cs
@@ -0,0 +1,100 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/**
+ * This is a model for the object Record (i.e. entry record in the csv table)
+ * @Author: Rodrigo Eltz
+ */
+namespace LanguageProjectAsp.Models
+{
+    public class Record
+    {
+        private int refDate;
+        private string geo;
+        private string dGuid;
+        private string sex;
+        private string ageGroup;
+        private string studentResponse;
+        private string uom;
+        private int uomId;
+        private string scalarFactor;
+        private int scalarId;
+        private string vector;
+        private string coordinate;
+        private string value;
+        private string status;
+        private string symbol;
+        private string terminated;
+        private double decimals;
+
+        /// Just passed to the constructors the values that are different. In the event of the dataset changing, this has to be adapted to construct based on new variables
+        public Record(string sex, string ageGroup, string studentResponse, string vector, string coordinate, string value)
+        {
+            this.refDate = 2002;
+            this.geo = "Canada";
+            this.dGuid = "";
+            this.sex = sex;
+            this.ageGroup = ageGroup;
+            this.studentResponse = studentResponse;
+            this.uom = "Percent";
+            this.uomId = 239;
+            this.scalarFactor = "units";
+            this.scalarId = 0;
+            this.vector = vector;
+            this.coordinate = coordinate;
+            this.value = value;
+            this.status = "";
+            this.symbol = "";
+            this.terminated = "";
+            this.decimals = 0d;
+        }
+
+        public int RefDate { get => refDate; set => refDate = value; }
+        public string Geo { get => geo; set => geo = value; }
+        public string DGuid { get => dGuid; set => dGuid = value; }
+        public string Sex { get => sex; set => sex = value; }
+        public string AgeGroup { get => ageGroup; set => ageGroup = value; }
+        public string StudentResponse { get => studentResponse; set => studentResponse = value; }
+        public string Uom { get => uom; set => uom = value; }
+        public int UomId { get => uomId; set => uomId = value; }
+        public string ScalarFactor { get => scalarFactor; set => scalarFactor = value; }
+        public int ScalarId { get => scalarId; set => scalarId = value; }
+        public string Vector { get => vector; set => vector = value; }
+        public string Coordinate { get => coordinate; set => coordinate = value; }
+        public string Value { get => value; set => this.value = value; }
+        public string Status { get => status; set => status = value; }
+        public string Symbol { get => symbol; set => symbol = value; }
+        public string Terminated { get => terminated; set => terminated = value; }
+        public double Decimals { get => decimals; set => decimals = value; }
+
+        /// This method parses the csv and creates an object of type Record for each line
+        public static Record FromCsv(string line)
+        {
+            var split = line.Split(',');
+            //System.Diagnostics.Debug.WriteLine("split 3 is "+ Convert.ToString(split[3]));
+            //System.Diagnostics.Debug.WriteLine("split 4 is " + Convert.ToString(split[4]));
+            //System.Diagnostics.Debug.WriteLine("split 5 is " + Convert.ToString(split[5]));
+            //System.Diagnostics.Debug.WriteLine("split 10 is " + Convert.ToString(split[10]));
+            //System.Diagnostics.Debug.WriteLine("split 11 is " + Convert.ToString(split[11]));
+            //System.Diagnostics.Debug.WriteLine("split 12 is " + Convert.ToString(split[12]));
+            Record record = new Record(Convert.ToString(split[3]),
+                                       Convert.ToString(split[4]),
+                                       Convert.ToString(split[5]),
+                                       Convert.ToString(split[10]),
+                                       Convert.ToString(split[11]),
+                                       Convert.ToString(split[12]));
+
+            return record;
+        }
+
+        public override string ToString()
+        {
+            return "Ref_Date: " + RefDate + " GEO: " + Geo + " Sex: " + Sex + " Age Group: " + AgeGroup
+                + " Student Response: " + StudentResponse + " Vector: " + Vector + " Coordinate:  " + Coordinate
+                + " Value: " + Value;
+        }
+    }
+}
